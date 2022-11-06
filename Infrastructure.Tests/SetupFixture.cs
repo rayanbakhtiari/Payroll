5011437462b115a57f6f22d85c975dfbf1d2761b
Hossein Bakhtiari <rayan.a.bakhtiari@gmail.com>
2022-11-06 13:04:31 +0800

    PaySlipFileRepositoryFactoryTests added to Infrastructure.Tests

---
+++ Infrastructure.Tests/SetupFixture.cs
@@ -0,0 +1,56 @@
+﻿using Application.Handlers.MonthlyPaySlip;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Infrastructure.Tests
+{
+    public class SetupFixture : IDisposable
+    {
+        private IServiceCollection? _services;
+
+
+        public SetupFixture()
+        {
+            SetCultureInfoToEnUS();
+            _services = new ServiceCollection();
+            _services.AddSingleton<ILoggerFactory, NullLoggerFactory>();
+            _services.BuildServiceProvider();
+
+        }
+        private void SetCultureInfoToEnUS()
+        {
+            var cultureInfo = new CultureInfo("en-US");
+
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+        }
+        public IPaySlipRepositoryFactory AddPaySlipFileRepositoryFactoryWithFileAddress(string inputFileAddress, string outputFileAddress)
+        {
+            if (_services is null)
+                _services = new ServiceCollection();
+            _services.AddPaySlipFileRepositoryForConsoleApp(inputFileAddress, outputFileAddress);
+            var builder = _services.BuildServiceProvider();
+            var factory = builder.GetService<IPaySlipRepositoryFactory>();
+            if(factory is null)
+            {
+                throw new InvalidOperationException("IPaySlipRepositoryFactory");
+            }
+            return factory;
+        }
+
+        public void Dispose()
+        {
+            _services?.Clear();
+            _services = null;
+        }
+    }
+}

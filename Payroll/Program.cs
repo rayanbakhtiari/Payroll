39b1940cb0a0565491de744965c81e97509b002c
Hossein Bakhtiari <rayan.a.bakhtiari@gmail.com>
2022-11-06 18:16:00 +0800

    refactor Program.cs

--- Payroll/Program.cs
+++ Payroll/Program.cs
@@ -3,45 +3,57 @@ using Infrastructure;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using System.Globalization;
 using System.Reflection;
-//Set CultureInfo to en-US in order to prevent CultureInfo at destination runtime.
-SetCultureInfoToEnUS();
-string outputCsvDirectory = $@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}/OutputData";
-if (!Directory.Exists(outputCsvDirectory))
-    Directory.CreateDirectory(outputCsvDirectory);
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddApplicationServices();
-builder.Services.AddPaySlipFileRepositoryWithInputStream(outputCsvDirectory);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    private static void Main(string[] args)
+    {
+        //Set CultureInfo to en-US in order to prevent CultureInfo at destination runtime.
+        SetCultureInfoToEnUS();
 
-app.UseHttpsRedirection();
+        string outputCsvDirectory = $@"{Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location)}/OutputData";
+        CreateCsvOutputDirectory(outputCsvDirectory );
+        var builder = WebApplication.CreateBuilder(args);
 
-app.UseAuthorization();
+        // Add services to the container.
 
-app.MapControllers();
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddApplicationServices();
+        builder.Services.AddPaySlipFileRepositoryWithInputStream(outputCsvDirectory);
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
 
 
-app.Run();
+        app.Run();
 
-void SetCultureInfoToEnUS()
-{
-    var cultureInfo = new CultureInfo("en-US");
+    }
+    private static void SetCultureInfoToEnUS()
+    {
+        var cultureInfo = new CultureInfo("en-US");
 
-    CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
-    CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+        CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+        CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+    }
+    private static void CreateCsvOutputDirectory(string path)
+    {
+        if (!Directory.Exists(path))
+            Directory.CreateDirectory(path);
+    }
 }
